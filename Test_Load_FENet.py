cff6e051969a10a1a26e7910020d5fa6d7d730e1
Exr0n <mail@exr0n.com>
2023-05-04 13:43:07 -0700

    test load old model---poor performance

--- Test_Load_FENet.py
+++ Test_Load_FENet.py
@@ -15,7 +15,7 @@ import pickle
 
 
 def test_data_maker():
-    _, _, withheld_test_dl = make_total_training_data(DATA_DIR, splits = ['test'], n_filtered_channels=None)
+    _, _, withheld_test_dl = make_total_training_data(DATA_DIR, splits = ['test'], n_filtered_channels=None, make_data_from_day_kwargs={ 'normalize_inputs': True })
     return withheld_test_dl
 if (LOAD_LOCAL_DATA_CACHE):
     data_pickle_name = f'total_training_data_minR2-{FILTERING_MIN_R2}_nchan-{FILTERING_MIN_R2}_WITHHELD_TEST.pkl'
@@ -25,6 +25,7 @@ else:
 
 def get_model_evals(path, test_dl):
     config, *_ = read_checkpoint(path)
+    config = { 'annealing_alpha': 0, 'anneal': False, 'thermal_sigma': 0, 'decoder': 0, 'pls_dims': 2, 'random_seed': 0, **config }
     device, _, pls_model, decoder, _, _, _, _, _  = initialize(run=run, config=config)
     fenet = make_fenet_from_checkpoint(path, device)    # don't use the fenet from initialize() because that one is not from the checkpoint
     evals = evaluate_with_criteria(fenet, pls_model, decoder, test_dl, [
@@ -33,7 +34,12 @@ def get_model_evals(path, test_dl):
     ], device)
     return evals, fenet
 
-with wandb.init(project="publishing_evaluation_ben", config=config) as run:
-    evals, _ = get_model_evals(path, withheld_test_dl)
-    print(evals)
+if __name__ == '__main__':
+    config, *_ = read_checkpoint(path)
+    with wandb.init(project="publishing_evaluation_ben", config=config, tags="oldcode-model") as run:
+        evals, _ = get_model_evals(path, withheld_test_dl)
+        print(evals)
+
+    with wandb.init(project="publishing_evaluation_ben") as run:
+        fenet = make_fenet_from_checkpoint('F:/Ben/wandb_saves')
 
